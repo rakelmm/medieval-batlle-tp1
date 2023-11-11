d337f2dfa4cc7c99e6c51670f1020ca28a386648
Rakel Moreira <rakelmoreiramendonca@gmail.com>
2023-11-10 21:25:34 -0300

    remoção de comentários, criação do README.md.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Medieval Battle - TP1 curso java
+
+Projeto criado para desenvolver o desafio do curso Java.
--- src/main/java/org/example/Personagem.java
+++ src/main/java/org/example/Personagem.java
@@ -14,55 +14,5 @@ public abstract class Personagem {
     private int pontosDeAgilidade;
     private String classe;
 
-
-//    public Personagem(int pontosDeVida, int pontosDeForca, int pontosDeAtaque, int pontosDeAgilidade, String classe) {
-//        this.pontosDeVida = pontosDeVida;
-//        this.pontosDeForca = pontosDeForca;
-//        this.pontosDeAtaque = pontosDeAtaque;
-//        this.pontosDeAgilidade = pontosDeAgilidade;
-//        this.classe = classe;
-//
-//    }
-
-//    public int getPontosDeVida() {
-//        return pontosDeVida;
-//    }
-//
-//    public void setPontosDeVida(int pontosDeVida) {
-//        this.pontosDeVida = pontosDeVida;
-//    }
-//
-//    public int getPontosDeForca() {
-//        return pontosDeForca;
-//    }
-//
-//    public void setPontosDeForca(int pontosDeForca) {
-//        this.pontosDeForca = pontosDeForca;
-//    }
-//
-//    public int getPontosDeAtaque() {
-//        return pontosDeAtaque;
-//    }
-//
-//    public void setPontosDeAtaque(int pontosDeAtaque) {
-//        this.pontosDeAtaque = pontosDeAtaque;
-//    }
-//
-//    public int getPontosDeAgilidade() {
-//        return pontosDeAgilidade;
-//    }
-//
-//    public void setPontosDeAgilidade(int pontosDeAgilidade) {
-//        this.pontosDeAgilidade = pontosDeAgilidade;
-//    }
-
     public abstract int getFatorDeDano();
-
-//    @Override
-//    public String toString() {
-//        return "Personagem{" +
-//                "pontosDeVida=" + pontosDeVida +
-//                ", classe='" + classe + '\'' +
-//                '}';
-//    }
 }
